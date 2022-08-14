74bb161b66eabd6911901a4418ba6ed720b0658f
Ted Phelps <phelps@gnusto.com>
2022-08-14 16:52:28 +1000

    Shift the struct time_zone information
    Most programs won't care about the details of the time zone struct, so
    let's shift that into tzfile.h.

--- tzfile.h
+++ tzfile.h
@@ -14,6 +14,24 @@ struct tz_header {
 };
 
 
+struct tz_offset {
+    int32_t utoff:23;
+    uint32_t isdst:1;
+    uint32_t desig:8;
+};
+
+
+struct time_zone {
+    uint32_t ts_count;
+    const int64_t *timestamps;
+    const uint8_t *offset_map;
+    const struct tz_offset *offsets;
+    const char *desig;
+    const char *tz;
+};
+
+
+
 void tz_header_fix_endian(struct tz_header *header);
 size_t tz_header_data_len(const struct tz_header *header, size_t time_size);
 
